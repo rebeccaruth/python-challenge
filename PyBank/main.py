55c392aae25e4774c741d1dade5b042cc2859fd8
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-08-21 14:00:52 -0500

    month total

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,22 @@
-#BANK
+#import csv
+import os
+import csv
+budget_csv = os.path.join('PyBank','Resources', 'budget_data.csv')
+with open(budget_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+
 
 #total number of months
+date = str(budget_csv[0])
+month_count = -1
+for row in open(budget_csv):
+    month_count += 1
+print('Total Months:', month_count)
+
 
 
 #net total of profit/losses
-
+#profit_loss = int(budget_csv[1])
 
 #average change of profit/ losses
 
