e319729894f99b47f812eed9730d38dfb03cb446
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-08-22 01:56:10 -0500

    month total

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,29 +4,38 @@ import csv
 budget_csv = os.path.join('PyBank','Resources', 'budget_data.csv')
 with open(budget_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csv_reader)
 
+#print analysis
+    print("Financial Analysis")
+    print("------------------------------------")
 
 #total number of months
-date = str(budget_csv[0])
-month_count = -1
-for row in open(budget_csv):
-    month_count += 1
-print('Total Months:', month_count)
-
-
+    month_count = -1
+    for row in open(budget_csv):
+        month_count += 1
+    print('Total Months:', month_count)
 
 #net total of profit/losses
-#profit_loss = int(budget_csv[1])
+    total = 0
+    #for row in budget_csv:
+       #total = total + int(row[1])
+    print('Total: ', total)
+
+
 
 #average change of profit/ losses
-
+    #average = total / (month_count - 1)
 
 #greatest increase
 
 
 
 #greatest decrease
+#def min()
 
 
-
-
+#OUTPUTS PRINTED
+    
+    
+   
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,14 @@
+#import csv
+import os
+import csv
+election_data = os.path.join('PyPoll', 'Resources', 'election_data.csv')
+with open(election_data) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
 
+#total votes
+vote_count = -1
+for row in election_data:
+    vote_count += 1
+print(vote_count)
 
 
