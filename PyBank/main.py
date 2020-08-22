b0a5974de8d7e06cbe4058755073ccb983f14510
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-08-22 04:05:38 -0500

    created text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,39 +3,58 @@ import os
 import csv
 budget_csv = os.path.join('PyBank','Resources', 'budget_data.csv')
 with open(budget_csv) as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csv_reader)
+    csvreader = csv.DictReader(csvfile, delimiter=',')
+    header = next(csvreader)
 
-#print analysis
-    print("Financial Analysis")
-    print("------------------------------------")
+#variables
+    month_total = 1
+    pl_total = 0
+    last_pl = 0
+    pl_change = 0
+    pl_change_list = []
+    month_change = []
+    greatest_inc = ["", 0]
+    greatest_dec = ["", 99999999]
 
-#total number of months
-    month_count = -1
-    for row in open(budget_csv):
-        month_count += 1
-    print('Total Months:', month_count)
+#loop
+    for row in csvreader:
 
-#net total of profit/losses
-    total = 0
-    #for row in budget_csv:
-       #total = total + int(row[1])
-    print('Total: ', total)
+        #totals
+        month_total = month_total + 1
+        pl_total = pl_total + int(row["Profit/Losses"])   
 
+        #revenue changes
+        pl_change = int(row["Profit/Losses"]) - last_pl
+        last_pl = int(row["Profit/Losses"])
+        month_change = month_change + [row["Date"]]
 
+        #greatest increase
+        if (pl_change > greatest_inc[1]):
+            greatest_inc[1] = pl_change
+            greatest_inc[0] = [row["Date"]]
 
-#average change of profit/ losses
-    #average = total / (month_count - 1)
+         #greatest decrease
+        if (pl_change < greatest_dec[1]):
+            greatest_dec[0] = [row["Date"]] 
+            greatest_dec[1] = pl_change
+               
+        
 
-#greatest increase
+#average        
+pl_avg = pl_total / month_total
 
+#print results
+output = (
+    f'Total Months: {month_total}\n'
+    f'Total: ${pl_total}\n'
+    f'Average Change: ${pl_avg}\n'
+    f'Greatest Increase in Profits: {greatest_inc}\n'
+    f'Greatest Decrease in Profits: {greatest_dec}\n'
+)
+print(output)
 
+#export to analysis
+analysis_txt = os.path.join('PyBank', 'Analysis', 'budget_analysis.txt')
+with open(analysis_txt, "w") as txt_file:
+    txt_file.write(output)
 
-#greatest decrease
-#def min()
-
-
-#OUTPUTS PRINTED
-    
-    
-   
